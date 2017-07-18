edd6b747eff6dd8dd1a8c39fd463951df206f2f5
nigeltiany <nigeltiany@outlook.com>
2017-07-18 02:30:36 -0400

    create different sized chat dialogs

--- client/components/chat.vue
+++ client/components/chat.vue
@@ -1,23 +1,64 @@
 <template>
     <div>
-        <v-speed-dial :fixed="true" :bottom="true" :right="true" :direction="'left'">
-            <v-btn slot="activator" class="blue darken-2" dark fab v-badge="{ value: '1', overlap: true, left: true }">
+        <v-dialog v-model="dialog" persistent hide-overlay class="hidden-sm-and-down">
+            <v-btn slot="activator" class="blue darken-2" fixed bottom right hover dark fab v-badge="{ value: '1', overlap: true, left: true }">
                 <v-icon>chat</v-icon>
                 <v-icon>close</v-icon>
             </v-btn>
-            <v-btn fab dark small class="green">
-                <v-icon>edit</v-icon>
+            <v-card id="chat-dialog">
+                <v-card-title>
+                    <div class="headline">Use Google's location service?</div>
+                </v-card-title>
+                <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn class="green--text darken-1" flat="flat" @click.native="dialog = false">Disagree</v-btn>
+                    <v-btn class="green--text darken-1" flat="flat" @click.native="dialog = false">Agree</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="dialogFS" persistent fullscreen class="hidden-md-and-up">
+            <v-btn slot="activator" class="blue darken-2" fixed bottom right hover dark fab v-badge="{ value: '1', overlap: true, left: true }">
+                <v-icon>chat</v-icon>
+                <v-icon>close</v-icon>
             </v-btn>
-        </v-speed-dial>
+            <v-card id="chat-dialog-fs">
+                <v-card-title>
+                    <div class="headline">Use Google's location service?</div>
+                </v-card-title>
+                <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn class="green--text darken-1" flat="flat" @click.native="dialogFS = false">Disagree</v-btn>
+                    <v-btn class="green--text darken-1" flat="flat" @click.native="dialogFS = false">Agree</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
 
-    export default {}
+    export default {
+        data: () => ({
+            dialog: false,
+            dialogFS: false
+        })
+    }
 
 </script>
 
-<style>
-
+<style lang="stylus">
+    .dialog__content
+        z-index 6
+    .dialog__content:nth-child(2)
+        display unset
+    .dialog--persistent
+        box-shadow unset !important
+    #chat-dialog
+        right: 16px;
+        bottom: 85px;
+        width: 350px;
+        position: absolute;
 </style>
