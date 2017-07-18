e59fd445354e5367a9366a2b9cc155b293a0082a
nigeltiany <nigeltiany@outlook.com>
2017-07-17 23:32:36 -0400

    add badge to chat fab

--- client/components/chat.vue
+++ client/components/chat.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-speed-dial :fixed="true" :bottom="true" :right="true" :direction="'left'">
-            <v-btn slot="activator" class="blue darken-2" dark fab hover v-model="fab">
+            <v-btn slot="activator" class="blue darken-2" dark fab v-badge="{ value: '1', overlap: true, left: true }">
                 <v-icon>chat</v-icon>
                 <v-icon>close</v-icon>
             </v-btn>
