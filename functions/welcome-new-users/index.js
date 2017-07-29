2de7c5d72e48a03083dca74942b64c8615dac092
nigeltiany <nigeltiany@outlook.com>
2017-07-28 23:35:56 -0400

    get available agent

--- functions/welcome-new-users/index.js
+++ functions/welcome-new-users/index.js
@@ -2,7 +2,14 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 
+const assign_user_customer_support = require('../assign-user-to-customer-support')
+
 module.exports = functions.database.ref('/users/{userID}').onCreate(event => {
+
+    var available_agent = assign_user_customer_support()
+
+    console.log(available_agent)
+
     return admin.database().ref('/messages/' + event.params.userID).set({
         title: 'Welcome to Lawn Care',
         sender: "Nigel Tiany",
