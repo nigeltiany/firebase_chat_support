9af511518b2894ee2812fbea254ac8c6d24e33fb
nigeltiany <nigeltiany@outlook.com>
2017-08-01 22:26:31 -0400

    issue conversation IDs for new chats for tracking

---
+++ functions/issue-chat-id/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+
+function guid(){
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,a=>(a^Math.random()*16>>a/4).toString(16))
+}
+
+module.exports = functions.database.ref('/messages/{User_ID}/{Message_ID}').onCreate(event => {
+    if (!event.data.hasChild('conversation_id')) {
+        return admin.database().ref('/messages/' + event.params.User_ID + '/' + event.params.Message_ID + '/conversation_id').set(guid())
+    }
+})
