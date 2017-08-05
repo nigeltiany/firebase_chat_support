02500d4213e9a23734edb16f8806fdf63b5cf009
nigeltiany <nigeltiany@outlook.com>
2017-08-05 13:31:19 -0400

    check message value before object keys

--- client/firebase/index.js
+++ client/firebase/index.js
@@ -45,11 +45,13 @@ export default new class firebase {
     onNewMessage(callback) {
         this.database.ref('messages/'+ this.user().uid).on('value', (userMessages) => {
             let messages = userMessages.val()
-            Object.keys(messages).map((message) => {
-                if(messages[message].conversation_id) {
-                    callback(messages[message])
-                }
-            })
+            if(messages) {
+                Object.keys(messages).map((message) => {
+                    if(messages[message].conversation_id) {
+                        callback(messages[message])
+                    }
+                })
+            }
         })
     }
 
