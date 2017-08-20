60c96ebd46ce93ee29e1fbc6b1f8c5e64d695d1d
nigeltiany <nigeltiany@outlook.com>
2017-08-20 17:38:36 -0400

    add messages without recipient to help queue

--- functions/issue-chat-id/index.js
+++ functions/issue-chat-id/index.js
@@ -2,13 +2,22 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 
-function guid(){
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,a=>(a^Math.random()*16>>a/4).toString(16))
-}
+const generate_uid = require('../guid')
 
 module.exports = functions.database.ref('/messages/{User_ID}/{Message_ID}').onCreate(event => {
     if (!event.data.hasChild('conversation_id')) {
+        const id = generate_uid()
         return admin.database().ref('/messages/' + event.params.User_ID + '/' + event.params.Message_ID)
-            .update({ conversation_id: guid() })
+            .update({ conversation_id: id })
+            .then(()=>{
+                let queued = event.data.val().to === null || undefined
+                return admin.database().ref('conversations/').child(id).set({
+                    members: [event.params.User_ID, !queued ? event.data.val().to : null],
+                }).then(() => {
+                    if(queued) {
+                        return admin.database().ref('help_queue/').push().set(event.data.val())
+                    }
+                })
+            })
     }
 })
