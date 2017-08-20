cde30d00be7c0ff98b9d825d48963ac5e26fc784
nigeltiany <nigeltiany@outlook.com>
2017-08-20 17:41:45 -0400

    assign welcome messages a conversation id

--- functions/welcome-new-users/index.js
+++ functions/welcome-new-users/index.js
@@ -2,6 +2,8 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 
+const generate_uid = require('../guid')
+
 const _sortBy = require('lodash.sortby'); const _isEqual = require('lodash.isequal'); const _intersection = require('lodash.intersection');
 
 module.exports = functions.database.ref('/users/{userID}').onCreate(event => {
@@ -21,17 +23,19 @@ module.exports = functions.database.ref('/users/{userID}').onCreate(event => {
 
             admin.database().ref('users/' + available_agent.uid + '/displayName').once("value", function(agent) {
                 let agentName = agent.val()
+                let conversation_uid = generate_uid()
 
                 return admin.database().ref('messages/' + event.params.userID).push().set({
                     auto: true, // Used to ignore sending message to agent until recipient replies
                     title: 'Welcome to Lawn Care',
                     sender: agentName,
-                    participants: [agentName],
-                    message: "Welcome to lawn care. Let us know if you have any questions",
-                    recipients: [event.params.userID, available_agent.uid]
+                    conversation_id: conversation_uid,
+                    participants: [agentName,],
+                    message: "Welcome to lawn care. Let us know if you have any questions"
                 }).then(() => {
                     let thisMoment = Date.now()
 
+                    //Update Customer Agent Relationship
                     return admin.database().ref('customer-support-agents/' + available_agent.uid).set(
                         Object.assign({}, available_agent, {
                             clients: available_agent.clients + 1,
@@ -39,7 +43,12 @@ module.exports = functions.database.ref('/users/{userID}').onCreate(event => {
                             lastClientAt: thisMoment,
                             updatedAt: thisMoment
                         })
-                    )
+                    // Then update conversations participants relationship
+                    ).then(() => {
+                        return admin.database().ref('conversations/').child(conversation_uid).set({
+                            members: [event.params.userID, available_agent.uid]
+                        })
+                    })
                 })
             })
 
