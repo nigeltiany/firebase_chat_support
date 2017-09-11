c8b412d06fa12da983b4fc8253aca135fb557057
nigeltiany <nigeltiany@outlook.com>
2017-09-11 15:30:46 -0400

    link user to conversations they are engaged in

--- functions/link-user-to-conversation/index.js
+++ functions/link-user-to-conversation/index.js
@@ -2,5 +2,14 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 module.exports = functions.database.ref('messages/{User_ID}/{Message_ID}/conversation_id').onCreate((event) => {
-    return admin.database().ref('user_conversations').child(event.params.User_ID).push({ conversation_id: event.data.val() })
+    return admin.database().ref('user_conversations')
+        .child(event.params.User_ID)
+        .child(event.data.val())
+        .child('messages').set({ [event.params.Message_ID]: true }).then(() => {
+            return admin.database().ref('user_conversations/' + event.params.User_ID + '/' + event.data.val())
+                .update({
+                    priority: 1 - Date.now(),
+                    updatedAt: Date.now()
+                })
+        })
 })
