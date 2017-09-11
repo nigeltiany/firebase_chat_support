4c7c62a13785f88e70bfd35f49b91132692e6474
nigeltiany <nigeltiany@outlook.com>
2017-09-11 16:43:24 -0400

    export track conversations as a function

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,4 @@ admin.initializeApp(functions.config().firebase);
 exports.welcome_new_user = require('./welcome-new-users')
 exports.messenger = require('./messenger')
 exports.user_conversation_linker = require('./link-user-to-conversation')
+exports.track_conversation_updates = require('./track-conversation-updates')
