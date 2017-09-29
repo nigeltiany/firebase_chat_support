d7802ea7f0c76a1d0c3e8bf4bc53649d21ac33c4
nigeltiany <nigeltiany@outlook.com>
2017-09-29 12:13:57 -0400

    create header with search field

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,22 +1,31 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <img src="v.png" alt="Vuetify.js" class="mb-5" />
-      </div>
-      <v-card>
-        <v-card-text>
-          <p>Welcome to the Webpack SSR template.</p>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications. For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>. If you have questions, please join the official <a href="https://gitter.im/vuetifyjs/Lobby" target="_blank" title="chat">gitter</a>. Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn primary flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
+      <span class="_brand_intro">
+        <h3 class="text-xs-center">
+          The easiest & most trusted brand in lawn care.
+        </h3>
+        <p class="text-xs-center">Get an easy free estimate</p>
+        <v-text-field label="Enter Address" single-line append-icon="search"></v-text-field>
+      </span>
     </v-flex>
   </v-layout>
 </template>
+
+<script lang="stylus">
+  ._brand_intro
+    position absolute
+    top 24.3%
+    -webkit-transform: translate(-50%, 0);
+    transform: translate(-50%, 0);
+    left: 50%;
+    h1
+      font-size 3em
+    p
+      color #757575
+      font-size 18px
+      font-weight 300
+      line-height 26px
+      margin 20px auto
+      max-width 500px
+</script>
